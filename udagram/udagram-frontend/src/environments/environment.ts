de7d6c65260b57126a03c82b97d423b9102809e7
ahmed elkhoky <ahmed.elkholy@orange.com>
2023-01-29 23:41:11 +0200

    circleci commit

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'udagram-api',
   apiHost: 'http://udagram-api-dev.eba-e3mbpymr.us-east-1.elasticbeanstalk.com/api/v0'
 };
